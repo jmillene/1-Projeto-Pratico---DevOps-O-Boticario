4b494aee95fced0fe11336b650be2c87f3e5e282
Jessica <jmillene12@gmail.com>
2024-04-24 15:16:05 -0300

    finalizando a função que solicita ao usuario a confimação do e-mail

--- scripts/user/entrada_usuario_bkp.py
+++ scripts/user/entrada_usuario_bkp.py
@@ -1,9 +1,11 @@
-import datetime
+from entrada_usuario_email_senha import interface_email
 
 
 def entrada_usuario_enviar_email():
     while True:
-        pergunta = str(input("Deseja enviar backup por e-mail? Sim[s] ou Não[n]? ")).lower()
+        pergunta = str(
+            input("Deseja enviar backup por e-mail? Sim[s] ou Não[n]? ")
+        ).lower()
         if pergunta == "s":
             interface_email()
             break
@@ -12,13 +14,5 @@ def entrada_usuario_enviar_email():
         else:
             print("Por favor, responda 's' ou 'n'.")
 
-def entrada_usuario_backup():
-    while True:
-        dia_backup = input("Informe a data para a realização do backup (DD/MM/YYYY): ")
-        hora_backup = input("Informe a hora para a realização do backup (HH:MM): ")
 
-        try:
-            data_hora_backup = datetime.datetime.strptime(f"{dia_backup} {hora_backup}", "%d/%m/%Y %H:%M")
-            return data_hora_backup
-        except ValueError:
-            print("Formato de data ou hora inválido. Certifique-se de inserir a data no formato DD/MM/YYYY e a hora no formato HH:MM.")
+entrada_usuario_enviar_email()
